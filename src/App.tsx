141efaa53dffcc34ee88436404952fe2373e4554
retronouns <3486507+retronouns@users.noreply.github.com>
2024-10-26 17:58:14 -0600

    add victor mono and basic frontpage

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,19 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <div className="w-screen h-screen grid place-items-center">
-        <div className="bg-slate-500">test</div>
+      <div className="w-screen h-screen flex flex-col gap-8 font-cas text-cas-text p-8">
+        <div className="flex flex-row gap-8 bg-cas-dark border-2 border-cas-text p-2">
+          <span className="font-bold flex-grow">Cas Codes</span>
+          <span>UNDER CONSTRUCTION!!!</span>
+        </div>
+        <div className="flex-grow flex flex-row gap-8">
+          <span className="flex-grow bg-cas-light border-2 border-cas-text p-2">
+            lorem ipsum dolor sit amet
+          </span>
+          <span className="flex-grow bg-cas-light border-2 border-cas-text p-2">
+            lorem ipsum dolor sit amet
+          </span>
+        </div>
       </div>
     </>
   );
