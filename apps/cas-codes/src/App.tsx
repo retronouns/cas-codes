02535975702f4815ea5173a98ee179a25076eadd
retronouns <3486507+retronouns@users.noreply.github.com>
2024-10-26 20:39:07 -0600

    begin monorepo

---
+++ apps/cas-codes/src/App.tsx
@@ -0,0 +1,25 @@
+// @deno-types="@types/react"
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex flex-col gap-8 font-cas text-cas-text p-8">
+        <div className="flex flex-row gap-8 bg-cas-dark border-2 border-cas-text p-2">
+          <span className="font-bold flex-grow">Cas Codes</span>
+          <span>UNDER CONSTRUCTION!!!</span>
+        </div>
+        <div className="flex-grow flex flex-row gap-8">
+          <span className="flex-grow bg-cas-light border-2 border-cas-text p-2">
+            lorem ipsum dolor sit amet
+          </span>
+          <span className="flex-grow bg-cas-light border-2 border-cas-text p-2">
+            lorem ipsum dolor sit amet
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
