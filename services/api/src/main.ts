485cff8d1d610e929f2a4e081533ebf60411187d
retronouns <3486507+retronouns@users.noreply.github.com>
2024-10-27 22:51:27 -0600

    trying again

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -8,6 +8,7 @@ await fastify.register(pg, { connectionString: PG_CONNECTION_STRING ?? "" });
 await fastify.register(cors, {
     origin: "*",
 });
+
 console.log(PG_CONNECTION_STRING);
 fastify.get("/", async (req, res) => {
     res.send({ message: "Hello world!" });
