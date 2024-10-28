94758a792428eff02ec731f91e80dc8d12240f07
retronouns <3486507+retronouns@users.noreply.github.com>
2024-10-28 01:41:27 -0600

    move some page elements around

--- apps/cas-codes/src/components/nav-left.tsx
+++ apps/cas-codes/src/components/nav-left.tsx
@@ -7,14 +7,6 @@ export const NavLeft = () => {
         <span className="flex flex-row md:flex-col gap-6 bg-cas-dark border-2 border-cas-text rounded-lg p-4 h-min min-w-fit">
             <NavLink to="/about-me">About Me</NavLink>
             <NavLink to="/links">Links</NavLink>
-            <div className="flex-grow"></div>
-            <Link
-                className="text-sm"
-                to="https://github.com/retronouns/cas-codes"
-                target="_blank"
-            >
-                ./src
-            </Link>
         </span>
     );
 };
