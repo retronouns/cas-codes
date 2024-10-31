1c00591dde837e36589359cf8175a34ce23ff5b1
retronouns <3486507+retronouns@users.noreply.github.com>
2024-10-30 19:44:16 -0600

    documentation

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ This is my porfolio website!
 
 The frontend is using Deno 2, React, and TailwindCSS. (`./apps/cas-codes` directory)
 
+[Frontend README](/apps/cas-codes/README.md)
+
 The backend is using Deno 2, Fastify, and Postgres. (`./services/api` directory)
+
+[Backend README](/services/api/README.md)
 
 Feel free to use it as an example of something simple built with the aforementioned tech stack!
 
--- services/spa-server/spa-server.ts
+++ services/spa-server/spa-server.ts
@@ -1,3 +1,11 @@
+/**
+ * This is just for deno deploy to serve the React app's build artifacts.
+ * Since this is a SPA, the only actual valid route is "/", which maps to `index.html`.
+ * This redirects requests to routes other than "/" to `index.html` as well so React can properly handle them.
+ *
+ * taken from an example workaround given in https://github.com/denoland/std/issues/3420
+ */
+
 import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
 Deno.serve(async (req) => {
