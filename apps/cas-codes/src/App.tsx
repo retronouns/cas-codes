90804d54b25544bd2f2622092da26405c6fc8321
retronouns <3486507+retronouns@users.noreply.github.com>
2024-10-30 18:37:49 -0600

    home page should redirect

--- apps/cas-codes/src/App.tsx
+++ apps/cas-codes/src/App.tsx
@@ -1,13 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { AboutMe } from "./pages/about-me.tsx";
 import { Links } from "./pages/links.tsx";
-import { Home } from "./pages/home.tsx";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/about-me" replace />} />
         <Route path="/about-me" element={<AboutMe />} />
         <Route path="/links" element={<Links />} />
       </Routes>
--- apps/cas-codes/src/pages/home.tsx
+++
@@ -1,9 +0,0 @@
-import { Page } from "../components/page.tsx";
-
-export const Home = () => {
-    return (
-        <Page>
-            <></>
-        </Page>
-    );
-};
