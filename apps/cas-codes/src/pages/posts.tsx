5d28b047a46f1e0a45ab5e0c9a4a12325a5b93a7
retronouns <3486507+retronouns@users.noreply.github.com>
2024-11-01 09:39:21 -0600

    put bsky posts in an array so its easier to add new ones

--- apps/cas-codes/src/pages/posts.tsx
+++ apps/cas-codes/src/pages/posts.tsx
@@ -1,16 +1,21 @@
 import { Page } from "../components/page.tsx";
 import { BlueskyEmbed } from "../components/bsky-embed.tsx";
 
+const POST_URLS = [
+    "https://bsky.app/profile/cas.bsky.social/post/3l7uiasat7k2e",
+    "https://bsky.app/profile/cas.bsky.social/post/3l7ugn6p4p326",
+    "https://bsky.app/profile/cas.bsky.social/post/3l7rkcrc75l2x",
+    "https://bsky.app/profile/cas.bsky.social/post/3l7qz6r4fgs26",
+    "https://bsky.app/profile/cas.bsky.social/post/3l7hztkcsbj2f",
+    "https://bsky.app/profile/cas.bsky.social/post/3l7hzcfwau62r",
+    "https://bsky.app/profile/cas.bsky.social/post/3l7fnszf56n2z",
+];
+
 export const Posts = () => {
     return (
         <Page>
             <div className="flex flex-col gap-6 pb-6">
-                <BlueskyEmbed postUrl="https://bsky.app/profile/cas.bsky.social/post/3l7ugn6p4p326" />
-                <BlueskyEmbed postUrl="https://bsky.app/profile/cas.bsky.social/post/3l7rkcrc75l2x" />
-                <BlueskyEmbed postUrl="https://bsky.app/profile/cas.bsky.social/post/3l7qz6r4fgs26" />
-                <BlueskyEmbed postUrl="https://bsky.app/profile/cas.bsky.social/post/3l7hztkcsbj2f" />
-                <BlueskyEmbed postUrl="https://bsky.app/profile/cas.bsky.social/post/3l7hzcfwau62r" />
-                <BlueskyEmbed postUrl="https://bsky.app/profile/cas.bsky.social/post/3l7fnszf56n2z" />
+                {POST_URLS.map((postUrl) => <BlueskyEmbed postUrl={postUrl} />)}
             </div>
         </Page>
     );
