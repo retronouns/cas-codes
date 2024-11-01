4fb91836e26e39911fe273e5ac0ce671fc719b32
retronouns <3486507+retronouns@users.noreply.github.com>
2024-10-31 23:18:47 -0600

    finished bsky embed component

--- apps/cas-codes/src/pages/posts.tsx
+++ apps/cas-codes/src/pages/posts.tsx
@@ -1,10 +1,12 @@
 import { Page } from "../components/page.tsx";
-import { BlueskyEmbed } from "../components/bsky.tsx";
+import { BlueskyEmbed } from "../components/bsky-embed.tsx";
 
 export const Posts = () => {
     return (
         <Page>
-            <BlueskyEmbed postUrl="https://bsky.app/profile/cas.bsky.social/post/3l7qz6r4fgs26" />
+            <div className="border-2 border-cas-text rounded-lg h-min bg-cas-light">
+                <BlueskyEmbed postUrl="https://bsky.app/profile/cas.bsky.social/post/3l7qz6r4fgs26" />
+            </div>
         </Page>
     );
 };
