d65c619a35265a0455454b0e4bf650cfc483b75c
retronouns <3486507+retronouns@users.noreply.github.com>
2024-11-09 00:50:07 -0700

    fix vite path alias

--- apps/cas-codes/vite.config.ts
+++ apps/cas-codes/vite.config.ts
@@ -3,8 +3,14 @@
 import { defineConfig } from "vite";
 import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react";
+import { resolve } from "jsr:@std/path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "./src/"),
+    },
+  },
   plugins: [deno(), react()],
 });
