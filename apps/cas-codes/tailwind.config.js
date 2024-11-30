fe8dab1cee6280501c65cc36303f090c6e405e17
retronouns <3486507+retronouns@users.noreply.github.com>
2024-11-29 17:10:31 -0700

    convert tailwind.config.ts to an esm module

--- apps/cas-codes/tailwind.config.js
+++ apps/cas-codes/tailwind.config.js
@@ -1,32 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        cas: {
-          "dark": "#f9688d",
-          "light": "#f8d0d0",
-          "bg": "#fbf0e8",
-          "text": "#534859",
-        },
-      },
-      keyframes: {
-        squiggle: {
-          "0%": { filter: "url(#squiggly-0)" },
-          "25%": { filter: "url(#squiggly-1)" },
-          "50%": { filter: "url(#squiggly-2)" },
-          "75%": { filter: "url(#squiggly-3)" },
-          "100%": { filter: "url(#squiggly-4)" },
-        },
-      },
-      animation: {
-        squiggle: "squiggle 0.4s infinite",
-      },
-      fontFamily: {
-        "cas": ["VictorMono", "monospace"],
+export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
+export const theme = {
+  extend: {
+    colors: {
+      cas: {
+        "dark": "#f9688d",
+        "light": "#f8d0d0",
+        "bg": "#fbf0e8",
+        "text": "#534859",
       },
     },
+    keyframes: {
+      squiggle: {
+        "0%": { filter: "url(#squiggly-0)" },
+        "25%": { filter: "url(#squiggly-1)" },
+        "50%": { filter: "url(#squiggly-2)" },
+        "75%": { filter: "url(#squiggly-3)" },
+        "100%": { filter: "url(#squiggly-4)" },
+      },
+    },
+    animation: {
+      squiggle: "squiggle 0.4s infinite",
+    },
+    fontFamily: {
+      "cas": ["VictorMono", "monospace"],
+    },
   },
-  plugins: [],
 };
+export const plugins = [];
